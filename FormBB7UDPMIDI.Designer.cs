8096f42e437d61ec7d8f0c80ce5632f06a642401
bbond007 <binarybond007@gmail.com>
2019-01-26 01:07:50 -0500

    Initial commit

---
+++ FormBB7UDPMIDI.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace UDPMIDI
+{
+    partial class FormBB7UDPMIDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxMidiDevice = new System.Windows.Forms.ComboBox();
+            this.textBoxPacketsRec = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUDPPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPacketsSent = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxMidiDevice
+            // 
+            this.comboBoxMidiDevice.FormattingEnabled = true;
+            this.comboBoxMidiDevice.Location = new System.Drawing.Point(114, 35);
+            this.comboBoxMidiDevice.Name = "comboBoxMidiDevice";
+            this.comboBoxMidiDevice.Size = new System.Drawing.Size(210, 21);
+            this.comboBoxMidiDevice.TabIndex = 3;
+            this.comboBoxMidiDevice.SelectedIndexChanged += new System.EventHandler(this.comboBoxMidiDevice_SelectedIndexChanged);
+            // 
+            // textBoxPacketsRec
+            // 
+            this.textBoxPacketsRec.Location = new System.Drawing.Point(114, 62);
+            this.textBoxPacketsRec.Name = "textBoxPacketsRec";
+            this.textBoxPacketsRec.ReadOnly = true;
+            this.textBoxPacketsRec.Size = new System.Drawing.Size(121, 20);
+            this.textBoxPacketsRec.TabIndex = 4;
+            this.textBoxPacketsRec.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Packets Received";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "MIDI Device";
+            // 
+            // textBoxUDPPort
+            // 
+            this.textBoxUDPPort.Location = new System.Drawing.Point(114, 114);
+            this.textBoxUDPPort.Name = "textBoxUDPPort";
+            this.textBoxUDPPort.ReadOnly = true;
+            this.textBoxUDPPort.Size = new System.Drawing.Size(121, 20);
+            this.textBoxUDPPort.TabIndex = 7;
+            this.textBoxUDPPort.Text = "1999";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "UDP Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Packets Sent";
+            // 
+            // textBoxPacketsSent
+            // 
+            this.textBoxPacketsSent.Location = new System.Drawing.Point(114, 88);
+            this.textBoxPacketsSent.Name = "textBoxPacketsSent";
+            this.textBoxPacketsSent.ReadOnly = true;
+            this.textBoxPacketsSent.Size = new System.Drawing.Size(121, 20);
+            this.textBoxPacketsSent.TabIndex = 9;
+            this.textBoxPacketsSent.Text = "0";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.labelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.Yellow;
+            this.labelTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(338, 22);
+            this.labelTitle.TabIndex = 11;
+            this.labelTitle.Text = "MiSTer MidiLink UDP Listener - BB7";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(295, 117);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(29, 19);
+            this.buttonReset.TabIndex = 12;
+            this.buttonReset.Text = "®";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // FormBB7UDPMIDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 154);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPacketsSent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUDPPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPacketsRec);
+            this.Controls.Add(this.comboBoxMidiDevice);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormBB7UDPMIDI";
+            this.Text = "UDPMIDI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormBB7UDPMIDI_FormClosing);
+            this.Load += new System.EventHandler(this.FormBB7UDPMIDI_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormBB7UDPMIDI_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBoxMidiDevice;
+        private System.Windows.Forms.TextBox textBoxPacketsRec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUDPPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPacketsSent;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ FormBB7UDPMIDI.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Net;
+using System.Drawing.Drawing2D;
+
+namespace UDPMIDI
+{
+    public partial class FormBB7UDPMIDI : Form
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MidiOutCaps
+        {
+            public UInt16 wMid;
+            public UInt16 wPid;
+            public UInt32 vDriverVersion;
+
+            [MarshalAs(UnmanagedType.ByValTStr,
+               SizeConst = 32)]
+            public String szPname;
+
+            public UInt16 wTechnology;
+            public UInt16 wVoices;
+            public UInt16 wNotes;
+            public UInt16 wChannelMask;
+            public UInt32 dwSupport;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MidiHdr
+        {
+            public IntPtr lpData;          // offset  0- 3
+            public uint dwBufferLength;    // offset  4- 7
+            public uint dwBytesRecorded;   // offset  8-11
+            public uint dwUser;            // offset 12-15
+            public uint dwFlags;           // offset 16-19
+            public IntPtr lpNext;          // offset 20-23
+            public IntPtr reserved;        // offset 24-27
+            public uint dwOffset;          // offset 28-31
+            public IntPtr dwReserved;      // offset 32-35
+        }
+
+        class MidiHdrFlag
+        {
+            public const int MHDR_DONE = 1;
+            public const int MHDR_PREPARED = 2;
+            public const int MHDR_INQUEUE = 4;
+            public const int MHDR_ISSTRM = 8;
+        }
+
+        // MCI INterface
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string command,
+        StringBuilder returnValue, int returnLength,
+        IntPtr winHandle);
+
+        // Midi API
+        [DllImport("winmm.dll")]
+        private static extern int midiOutGetNumDevs();
+
+        [DllImport("winmm.dll")]
+        private static extern int midiOutGetDevCaps(Int32 uDeviceID,
+           ref MidiOutCaps lpMidiOutCaps, UInt32 cbMidiOutCaps);
+
+        [DllImport("winmm.dll")]
+        private static extern int midiOutOpen(ref int handle,
+           int deviceID, MidiCallBack proc, int instance, int flags);
+
+        [DllImport("winmm.dll")]
+        private static extern int midiOutShortMsg(int handle,
+           int message);
+
+        [DllImport("winmm.dll")]
+        private static extern int midiOutClose(int handle);
+
+        private delegate void MidiCallBack(int handle, int msg,
+          int instance, int param1, int param2);
+
+        [DllImport("winmm.dll")]
+        static extern int midiOutLongMsg(int hMidiOut, ref MidiHdr lpMidiOutHdr, int uSize);
+
+        [DllImport("winmm.dll")]
+        static extern int midiOutPrepareHeader(int hMidiOut, ref MidiHdr lpMidiOutHdr, int uSize);
+
+        [DllImport("winmm.dll")]
+        static extern int midiOutUnprepareHeader(int hMidiOut, ref MidiHdr lpMidiOutHdr, int uSize);
+
+        public FormBB7UDPMIDI()
+        {
+            InitializeComponent();
+        }
+
+        const string MT32_Message = "<<<Binary Bond007>>>";
+
+        void SendLongMsg(int systemHandle, byte[] data)
+        {
+            if (data.Length > (64 * 1024))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            //----------// MidiHdr
+            int hdrSize = Marshal.SizeOf(typeof(MidiHdr));
+            byte[] hdrReserved = new byte[8];
+            MidiHdr hdr = new MidiHdr();
+            GCHandle handle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            GCHandle revHandle = GCHandle.Alloc(hdrReserved, GCHandleType.Pinned);
+            //----------// MidiHdr
+            hdr.lpData = handle.AddrOfPinnedObject();
+            hdr.dwBufferLength = (uint)data.Length;
+            hdr.dwFlags = 0;
+            //----------//
+            midiOutPrepareHeader(systemHandle, ref hdr, hdrSize);
+            while ((hdr.dwFlags & MidiHdrFlag.MHDR_PREPARED) != MidiHdrFlag.MHDR_PREPARED)
+            {
+                Thread.Sleep(1);
+            }
+            midiOutLongMsg(systemHandle, ref hdr, hdrSize);
+            while ((hdr.dwFlags & MidiHdrFlag.MHDR_DONE) != MidiHdrFlag.MHDR_DONE)
+            {
+                Thread.Sleep(1);
+            }
+            midiOutUnprepareHeader(systemHandle, ref hdr, hdrSize);
+            //----------// GCHandle
+            handle.Free();
+            revHandle.Free();
+            //----------//
+        }
+
+        void OnUdpData(IAsyncResult result)
+        {
+            UdpClient socket = result.AsyncState as UdpClient;
+            // points towards whoever had sent the message:
+            IPEndPoint source = new IPEndPoint(0, 0);
+            byte[] message = socket.EndReceive(result, ref source);
+            SendLongMsg(midiHandle, message);
+
+            Invoke((MethodInvoker)delegate
+            {
+                try
+                {
+                    textBoxPacketsRec.Text = inCount++.ToString();
+                }
+                catch (Exception OOPS)
+                { }
+            });
+
+            socket.BeginReceive(new AsyncCallback(OnUdpData), socket);
+        } 
+
+        int midiHandle = 0;
+        int inCount = 0;
+
+        private void OpenMIDIDevice()
+        {
+            if (midiHandle != 0)
+                midiOutClose(midiHandle);
+            int result = midiOutOpen(ref midiHandle, comboBoxMidiDevice.SelectedIndex, null, 0, 0);
+            SetMT32_LCD(MT32_Message);
+        }
+
+        private void FormBB7UDPMIDI_Load(object sender, EventArgs e)
+        {
+            var numDevs = midiOutGetNumDevs();
+            if (numDevs > 0)
+            {
+                for (int i = 0; i < numDevs; i++)
+                {
+                    MidiOutCaps myCaps = new MidiOutCaps();
+                    int result2 = midiOutGetDevCaps(i, ref myCaps, (UInt32)Marshal.SizeOf(myCaps));
+                    comboBoxMidiDevice.Items.Add(string.Format("{0} - {1}", i, myCaps.szPname));
+                }
+                if (numDevs > 0)
+                    comboBoxMidiDevice.SelectedIndex = numDevs - 1;
+
+                UdpClient socket = new UdpClient(int.Parse(textBoxUDPPort.Text));
+                IPAddress multicastaddress = IPAddress.Parse("239.0.0.1");
+                socket.JoinMulticastGroup(multicastaddress);
+                socket.BeginReceive(new AsyncCallback(OnUdpData), socket); 
+            }
+            else
+            {
+                MessageBox.Show("ERROR : NO MIDI DEVICES FOUND");
+            }
+            textBoxPacketsRec.ReadOnly = true;
+        }
+
+        private void SetMT32_LCD(string MT32Message)
+        {
+            byte[] buf = {0xF0, 0x41, 0x10, 0x16, 0x12, 0x20, 0x00, 0x00,
+                          0,0,0,0,0, //sysex character data
+                          0,0,0,0,0, // "
+                          0,0,0,0,0, // "
+                          0,0,0,0,0, // "
+                          0x00, /* checksum placedholder */
+                          0xF7  /* end of sysex */ };
+            int checksum = 0;
+            int MT32MessageIndex = 0;
+            for (int bufIndex = 5; bufIndex < buf.Length - 2; bufIndex++)
+            {
+                if (bufIndex > 7)
+                {
+                    if (MT32MessageIndex < MT32Message.Length)
+                        buf[bufIndex] = (byte)MT32Message[MT32MessageIndex++];
+                    else
+                        buf[bufIndex] = 0x20;
+                }
+                checksum += buf[bufIndex];
+            }
+            checksum = 128 - checksum % 128;
+            buf[buf.Length - 2] = (byte)(checksum);
+            SendLongMsg(midiHandle, buf);
+        }
+
+        private void FormBB7UDPMIDI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (midiHandle != 0)
+                midiOutClose(midiHandle);
+        }
+
+        private void comboBoxMidiDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OpenMIDIDevice();
+        }
+
+        private void FormBB7UDPMIDI_Paint(object sender, PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                                                              Color.WhiteSmoke,
+                                                              Color.SteelBlue,
+                                                              90F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+            labelTitle.Refresh();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            inCount = 0;
+            textBoxPacketsRec.Text = "0";
+            textBoxPacketsSent.Text = "0";
+            SetMT32_LCD(MT32_Message);
+        }
+    }
+}
